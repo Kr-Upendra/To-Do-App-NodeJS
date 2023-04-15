f3d3896f8fbe00c9879b35bdbb06b8a624553a64
Upendra Kumar <toxic74412@gmail.com>
2023-04-15 22:21:10 +0530

    Completed frontend

--- Frontend/script.js
+++ Frontend/script.js
@@ -4,6 +4,13 @@ const todoStatus = document.querySelectorAll(".todostatus");
 const label = document.querySelectorAll("label");
 const icons = document.querySelectorAll(".ccicons");
 const radioButtons = document.querySelectorAll(".radiobtn");
+const deleteBtn = document.querySelectorAll(".todo__delete--icon");
+const loader = document.querySelector(".loader");
+const submitBtn = document.querySelector(".submit__btn");
+const alertBox = document.querySelector(".alertbox");
+const alertBtn = document.querySelector(".alert__confirm");
+const alertMessage = document.querySelector(".alert__message");
+const todo = document.querySelectorAll(".todo");
 
 const radioFunction = () => {
   for (let i = 0; i < icons.length; i++) {
@@ -11,9 +18,11 @@ const radioFunction = () => {
       if (radioButtons[i].value === "completed") {
         icons[i].setAttribute("data-iscompleted", "true");
         icons[i + 1].setAttribute("data-iscanceled", "false");
+        fillBrackets();
       } else if (radioButtons[i].value === "canceled") {
         icons[i].setAttribute("data-iscanceled", "true");
         icons[i - 1].setAttribute("data-iscompleted", "false");
+        fillBrackets();
       }
     });
   }
@@ -23,25 +32,21 @@ radioFunction();
 
 const fillBrackets = () => {
   for (let i = 0; i < todoStatus.length; i++) {
-    if (completeInputIcon[i].getAttribute("data-iscompleted") === "true")
+    if (completeInputIcon[i].getAttribute("data-iscompleted") === "true") {
       todoStatus[i].innerHTML = "[&#x2714;]";
-    else if (cancelInputIcon[i].getAttribute("data-iscanceled") === "true")
+    } else if (cancelInputIcon[i].getAttribute("data-iscanceled") === "true") {
       todoStatus[i].innerHTML = "[&#x292B;]";
-    else todoStatus.innerHTML = "[]";
+    } else todoStatus.innerHTML = "[]";
   }
 };
 
-fillBrackets();
+// const showAlert = () => {
+//   alertBox.style.visibility = "visible";
+//   alertMessage.innerHTML = "To do added successfully!";
+// };
 
-const loader = document.querySelector(".loader");
-const submitBtn = document.querySelector(".submit__btn");
+// const hideAlert = () => {
+//   alertBox.style.visibility = "hidden";
+// };
 
-const activateLoader = () => {
-  submitBtn.addEventListener("submit", () => {
-    setTimeout(() => {
-      loader.style.visibility = "visible";
-    }, 3000);
-  });
-};
-
-activateLoader();
+// const sets = setTimeout(hideAlert, 3000);
